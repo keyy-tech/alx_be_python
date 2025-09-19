9d92234b2c7444c1799c3abcfd2c40689c612fad
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-09-19 12:55:48 +0000

    Weather Advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,16 +1,12 @@
-# Ask the user for the weather condition
-weather_condition = input("What's the weather like today? (sunny/rainy/cloudy): ")
+# Prompt the user for input
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-# Control flow to check the weather condition and give advice
-# if statement for sunny conditions
-if weather_condition == "sunny":
+# Conditional checks
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-# elif statement for rainy conditions
-elif weather_condition == "rainy":
-    print("Don't forget your umbrella. and a raincoat.")
-# elif statement for cloudy conditions
-elif weather_condition == "cloudy":
-    print("Make sure to wear a warm coat and a scarf")
-# else statement for any other conditions
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, I don't have a recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
