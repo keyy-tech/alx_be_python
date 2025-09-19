bf1556927ea66b470101d29a53d64617820e11e8
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-09-19 13:12:26 +0000

    Made somee changes

--- control-flow/match_case_calulator.py
+++ control-flow/match_case_calulator.py
@@ -1,31 +1,29 @@
-# Taking two numbers and an operator as input from the user
+# match_case_calculator.py
+
+# Taking two numbers as input from the user
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Prompt the user for the operator
-operator = input("Choose the operator (+,-,*./): ")
-
-if num1 == 0 or num2 == 0:
-    print("Cannot divide by zero.")
-    exit()
+operator = input("Choose the operation (+, -, *, /): ")
 
 # Using a match-case to perform the operation based on the operator
 match operator:
     case "+":
         result = num1 + num2
-        print(f"The result is: {result}")
+        print(f"The result is {result}.")
     case "-":
         result = num1 - num2
-        print(f"The result is: {result}")
+        print(f"The result is {result}.")
     case "*":
         result = num1 * num2
-        print(f"The result is: {result}")
+        print(f"The result is {result}.")
     case "/":
-        # Using if-else to handle the division by zero
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
             result = num1 / num2
-            print(f"The result is: {result}")
+            print(f"The result is {result}.")
     case _:
         print("Invalid operator. Please try again.")
+
