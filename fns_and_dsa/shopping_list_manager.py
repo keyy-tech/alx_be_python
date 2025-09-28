077206cc3326445047824dfa13c2a8b2abfcd767
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-09-28 01:23:07 +0000

    Just created a shopping managing application with python

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,40 @@
+def display_menu():
+    print("\nShopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Clear List")
+    print("5. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        match choice:
+            case "1":
+                item = input("Enter item name: ")
+                shopping_list.append(item)
+                print(f"'{item}' has been added to the list.")
+                print(shopping_list)
+            case "2":
+                item = input("Enter item name to remove: ")
+                if item in shopping_list:
+                    shopping_list.remove(item)
+                    print(f"'{item}' has been removed from the list.")
+                else:
+                    print(f"'{item}' not found in the list.")
+            case "3":
+                print("Shopping List:")
+                print(shopping_list)
+            case "4":
+                shopping_list.clear()
+                print("Shopping list cleared.")
+            case "5":
+                print("Goodbye!")
+                break
+            case _:
+                print("Invalid choice.")
+
+if __name__ == "__main__":
+    main()
