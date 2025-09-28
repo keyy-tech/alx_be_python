43676c1a0acf93c6ba769ccb4dffa0bf350a82a8
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-09-28 01:02:59 +0000

    Created a function with arthimetric operations in python

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1:float, num2:float, operation):
+    """Performs the specified arithmetic operation on two numbers."""
+
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operation"
